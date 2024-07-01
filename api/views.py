f072bfa071560bd2bb14f26d91efdcbb15ec70ec
precious <jkpresh@gmail.com>
2024-07-01 12:57:51 +0100

    changed api

--- api/views.py
+++ api/views.py
@@ -6,37 +6,27 @@ from django.conf import settings
 from .serializers import HelloSerializer
 import requests
 
-def get_location(ip):
-    """Retrieve location information based on the IP address."""
+
+def get_weather_and_location(ip):
+    """Retrieve weather and location information based on the IP address."""
+    api_key = settings.WEATHERAPI_KEY
     try:
-        response = requests.get(f"http://ip-api.com/json/{ip}")
+        response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ip}")
         response.raise_for_status()
         data = response.json()
-        return data
+        city = data['location']['name']
+        temperature = data['current']['temp_c']
+        return city, temperature
     except requests.exceptions.RequestException as e:
-        print(f"Error retrieving location: {e}")
-        return {}
-    
-    
-def get_temperature(city):
-    """Retrieve temperature information for the specified city."""
-    api_key =settings.OPENWEATHERMAP_API_KEY
-    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}")
-    data = response.json()
-    if response.status_code == 200:
-        return data['main']['temp']
-    return None
-
+        print(f"Error retrieving weather and location data: {e}")
+        return 'Unknown', None
 
 @api_view(['GET'])
 def hello(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
     client_ip = request.META.get('REMOTE_ADDR', '8.8.8.8')
 
-    location = get_location(client_ip)
-    city = location.get('city', 'Unknown')
-
-    temperature = get_temperature(city)
+    city, temperature = get_weather_and_location(client_ip)
     if temperature is None:
         temperature = "unknown"
     
