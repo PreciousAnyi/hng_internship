fca2362afa639c51881b989f3e6f24e4ccc892c3
Precious <jkpresh@gmail.com>
2024-07-01 20:57:27 +0100

    modified file

--- api/views.py
+++ api/views.py
@@ -10,27 +10,32 @@ import urllib.parse
 
 def sanitize_input(input_string):
     """Sanitize and clean user input."""
-    # Decode URL-encoded characters
     input_string = urllib.parse.unquote(input_string)
-    # Remove surrounding quotes if they exist
     if input_string.startswith('"') and input_string.endswith('"'):
         input_string = input_string[1:-1]
     return input_string
 
-def get_weather_and_location(location):
-    """Retrieve weather and location information based on the IP address."""
+def get_city_from_ip(ip):
+    """Retrieve city name based on the IP address."""
     try:
-        if location.city:
-            city = location.city
+        location = geocoder.ip(ip)
+        if location.ok:
+            return location.city or 'Unknown'
         else:
-            city = "unknown"
-        api_key = settings.WEATHERAPI_KEY
+            return 'Unknown'
+    except Exception as e:
+        print(f"Error retrieving city from IP: {e}")
+        return 'Unknown'
+
+def get_weather_and_location(city):
+    """Retrieve weather information based on the city."""
+    api_key = settings.WEATHERAPI_KEY
+    try:
         response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no")
         response.raise_for_status()
         data = response.json()
         temperature = data['current']['temp_c']
         return city, temperature
-    
     except (requests.exceptions.RequestException, KeyError, ValueError) as e:
         print(f"Error retrieving weather and location data: {e}")
         return 'Unknown', None
@@ -40,11 +45,18 @@ def hello(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
     visitor_name = sanitize_input(visitor_name)
     
-    # Determine client IP using geocoder
-    location = geocoder.ip(request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR')))
+    # Determine client IP
     client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
+    if isinstance(client_ip, list):
+        client_ip = client_ip[0].strip()
+    elif client_ip:
+        client_ip = str(client_ip).strip()
+    else:
+        client_ip = '8.8.8.8'  # Fallback to a default IP if not found
 
-    city, temperature = get_weather_and_location(location)
+
+    city = get_city_from_ip(client_ip)
+    city, temperature = get_weather_and_location(city)
     if temperature is None:
         temperature = "unknown"
     
